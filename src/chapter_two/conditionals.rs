b1fbaf5f80eda7dde473f01f66a5a69cafff5df0
Madebyshedrack <madebyshedrack@Madebyshedracks-MacBook-Pro.local>
2026-05-13 13:59:43 -0700

    done

--- src/chapter_two/conditionals.rs
+++ src/chapter_two/conditionals.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 pub fn conditional_basics() {
     let status = "single";
 
@@ -40,4 +42,58 @@ pub fn conditional_basics() {
             println!("{} is odd number", item);
         }
     }
+
+    let x = 49;
+
+    match x {
+        0 => {
+            println!("found zero");
+        }
+        1 | 2 => {
+            println!("found 1 or 2");
+        }
+        3..=9 => {
+            println!("found a number 3 to 9 inclusively");
+        }
+        matched_num @ 10..=100 => {
+            println!("Found {} number between 10 to 100", matched_num);
+        }
+        _ => {
+            println!("found something else");
+        }
+    }
+
+    let mut x = 0;
+
+    let value = loop {
+        x += 1;
+        if x == 13 {
+            break "found the 13";
+        }
+    };
+    println!("from loop: {}", value);
+    println!("from function: {}", iterator_logic());
+}
+
+fn iterator_logic() -> i32 {
+    let iterator = 42;
+
+    let value = if iterator < 42 { -1 } else { 1 };
+    println!("from if: {}", value);
+
+    let food = "Hamburger";
+    let result = match food {
+        "hotdog" => "is hotdog",
+        _ => "is not working",
+    };
+    println!("Identifying food: {}", result);
+
+    let value = {
+        let a = 1;
+        let b = 2;
+        a + b
+    };
+    println!("from block: {}", value);
+
+    value + 4
 }
