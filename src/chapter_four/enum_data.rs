3080ab7d813f5746141c28d16bceeed0fd9aaf69
Madebyshedrack <madebyshedrack@Madebyshedracks-MacBook-Pro.local>
2026-05-14 08:12:49 -0700

    done

--- src/chapter_four/enum_data.rs
+++ src/chapter_four/enum_data.rs
@@ -1,1 +1,27 @@
-pub fn enum_data_basics() {}
+pub fn enum_data_basics() {
+    let txn = Payment {
+        payment_type: String::from("ACH"),
+        uuid: 0012,
+        is_atomic: false,
+        transaction_status: Transactions::Failed(String::from("Instant"), 12),
+    };
+
+    println!(
+        "Payment: {} -> UUID: {} -> Atomicity: {} -> Status: {:#?}",
+        txn.payment_type, txn.uuid, txn.is_atomic, txn.transaction_status
+    );
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+enum Transactions {
+    Failed(String, u32),
+    Successful,
+}
+
+struct Payment {
+    payment_type: String,
+    uuid: u32,
+    is_atomic: bool,
+    transaction_status: Transactions,
+}
